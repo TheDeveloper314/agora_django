7661a4a6beac082fe14fe39fc5e0b110882e7607
unknown <tariqwaleed27@gmail.com>
2024-09-06 13:05:17 +0300

    Added profile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from .models import User
+from .models import User, UserProfile
 from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
 class CustomUserAdmin(UserAdmin):
 	list_display = ("email", "first_name", "is_active")
-	# ordering = ("-date_joined",)
+	ordering = ("-date_joined",)
 	filter_horizontal = ()
 	list_filter = ()
 	fieldset = ()
 
-admin.site.register(User, CustomUserAdmin)
+# admin.site.register(User, CustomUserAdmin)
+admin.site.register(User)
+admin.site.register(UserProfile)
